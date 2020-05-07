3837a74661049ac76e765dc83ff104cc8ba34e55
L A Fitzgerald <lyndonf@thomasdev.co.uk>
2020-05-07 14:26:06 +0100

    Update DrupalService.js

--- src/services/DrupalService.js
+++ src/services/DrupalService.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const getData = async () => {
-  const response = await axios.get("http://thomasproductdev.prod.acquia-sites.com/en/outcomes/ppa/customer/leadershipstyle/cd")  
+  const response = await axios.get("https://thomasproductdev.prod.acquia-sites.com/en/outcomes/ppa/customer/leadershipstyle/cd")  
   const paras = response.data[0];
   debugger;
   return paras;
